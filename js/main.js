30c9e46f87bc3dc405547d0406dc3f295b1e1ea5
you55efmansour <mlmleg2017@gmail.com>
2024-01-20 18:11:31 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+let inputText = document.querySelector(".input")
+let btn = document.querySelector(".add")
+let taskZone = document.querySelector(".tasks")
+let tasksArray=[]
+
+if (localStorage.getItem("task")) {
+    tasksArray = JSON.parse(localStorage.getItem("task"));
+}
+
+getTasks()
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    if (inputText.value == "") {
+        
+    }else{
+        addTask(inputText.value)
+        inputText.value= ""
+    }
+})
+
+// add task to array
+
+function addTask(text){
+    const task = {
+        id: Date.now(),
+        body: text,
+        done: false,
+    };
+    
+    tasksArray.push(task)
+    addToLocal(tasksArray)
+    addTaskToPage(tasksArray)
+}
+
+// add task to task zone
+
+function addTaskToPage(tasksArray){
+    taskZone.innerHTML=""
+    for (let i = 0; i < tasksArray.length; i++) {
+        let taskDiv = document.createElement("div")
+        taskDiv.classList.add("task")
+        if (tasksArray[i].done) {
+            taskDiv.classList.add("done")
+        }
+        taskDiv.setAttribute("task-id",tasksArray[i].id)
+        let textD =  document.createElement("div")
+        textD.append(tasksArray[i].body)
+        taskDiv.append(textD)
+        let dSpan = document.createElement("span")
+        dSpan.className="delete"
+        dSpan.append("delete")
+        taskDiv.append(dSpan)
+        taskZone.appendChild(taskDiv)
+    }
+}
+
+
+// add tasks to local storage 
+
+function addToLocal(tasksArray) {
+    window.localStorage.setItem("task",JSON.stringify(tasksArray))
+}
+
+// get tasks at the tasks zone 
+function getTasks() {
+    const tasks = JSON.parse(window.localStorage.getItem("task"))
+    
+    if (tasks) {
+        addTaskToPage(tasks) 
+    }
+}
+
+
+
+taskZone.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("task")) {
+        addDone(e.target.getAttribute("task-id"))
+    }
+    if (e.target.classList.contains("delete")) {
+        removeTask(e.target.parentElement.getAttribute("task-id"))
+        e.target.parentElement.remove()
+    }
+})
+
+// add done to task 
+function addDone(targetId){
+        for (let i = 0; i < tasksArray.length; i++) {
+            if (targetId == tasksArray[i].id) {
+                tasksArray[i].done === false ? tasksArray[i].done = true:tasksArray[i].done = false;
+            }
+        }
+        addTaskToPage(tasksArray)
+        addToLocal(tasksArray)
+}
+
+// remove task 
+
+function removeTask(taskId){
+    tasksArray = tasksArray.filter((task)=> task.id != taskId )
+    addToLocal(tasksArray)
+}
